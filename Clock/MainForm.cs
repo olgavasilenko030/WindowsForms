d849e535f1183934bcf0756fb671811e26b6f3ea
Vasil_th18@top-academy.ru <Vasil_th18@top-academy.ru>
2024-12-13 00:01:24 +0500

    Add project files.

---
+++ Clock/MainForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void timer_Tick(object sender, EventArgs e)
+		{
+			labelTime.Text = DateTime.Now.ToString("hh:mm:ss tt", System.Globalization.CultureInfo.InvariantCulture); //"HH:mm:ss" В 24 часовом формате
+																   // https://learn.microsoft.com/ru-ru/dotnet/api/system.globalization.cultureinfo.invariantculture?view=net-8.0
+		if (cbShowDate.Checked)
+			{
+				labelTime.Text += "\n";
+				labelTime.Text += DateTime.Now.ToString("yyyy.MM.dd");//"d"  дата по умолчанию
+			}
+		}
+
+		private void btnHideControls_Click(object sender, EventArgs e)
+		{
+			cbShowDate.Visible = false;
+			btnHideControls.Visible = false;
+			this.TransparencyKey = this.BackColor;
+			this.FormBorderStyle = FormBorderStyle.None;
+			labelTime.BackColor = Color.AliceBlue;
+			this.ShowInTaskbar = false;
+		}
+
+		private void labelTime_DoubleClick(object sender, EventArgs e)
+		{
+			MessageBox.Show
+				(this,
+				"Вы два раза щелкнули мышью по времени. и теперь вы управляете временем",
+				"info",
+				MessageBoxButtons.OK,
+				MessageBoxIcon.Information
+				);
+		}
+	}
+}
